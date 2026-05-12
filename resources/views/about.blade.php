23dbe1e7271d3e9f60f48c3d517acbff7320918e
Farah alnaouq <farahalnaouq@gmail.com>
2026-05-12 14:26:51 +0300

    Pass departments array to about view and display using foreach

--- resources/views/about.blade.php
+++ resources/views/about.blade.php
@@ -1,2 +1,9 @@
 <h1>About Page</h1>
 <p>Welcome, {{ $name }}!</p>
+
+<h3>Our Departments:</h3>
+<select name="department">
+    @foreach($departments as $key => $value)
+        <option value="{{ $key }}">{{ $value }}</option>
+    @endforeach
+</select>
--- routes/web.php
+++ routes/web.php
@@ -3,6 +3,11 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/about', function () {
-    $name = "Farah"; 
-    return view('about')->with('name', $name);
+    $name = "Farah";
+    $departments = [
+        '1' => 'Technical',
+        '2' => 'Financial',
+        '3' => 'Sales'
+    ];
+    return view('about', compact('name', 'departments'));
 });
