062ecf0652a01f78a6adf9ed7ca64de0e09df762
Farah alnaouq <farahalnaouq@gmail.com>
2026-05-12 14:16:40 +0300

    Create about page and pass data from route to view

--- routes/web.php
+++ routes/web.php
@@ -2,6 +2,7 @@
 
 use Illuminate\Support\Facades\Route;
 
-Route::get('/', function () {
-    return view('welcome');
+Route::get('/about', function () {
+    $name = "Farah"; 
+    return view('about')->with('name', $name);
 });
